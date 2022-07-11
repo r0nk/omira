ad6a0f7ada498d97aed37d1eb3c9ed4e4ab98869
r0nk <r0nk@simplecpu.com>
2022-07-11 16:41:48 -0500

    removed old stuff from finish, specific dates for discipline.

--- cmd/discipline.go
+++ cmd/discipline.go
@@ -11,16 +11,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var date_string string
+
 // disciplineCmd represents the discipline command
 var disciplineCmd = &cobra.Command{
 	Use:   "discipline",
 	Short: "Print the percentage of tasks complete today.",
 	Long:  `Output the percentage of tasks complete to day, to be used in scripts or conkys.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%f\n", state.Discipline)
+		fmt.Printf("%f\n", state.Discipline(state.Get_date(date_string)))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(disciplineCmd)
+	addCmd.Flags().StringVarP(&date_string, "date", "d", "", "Set the date to get discipline for.")
 }
--- cmd/finish.go
+++ cmd/finish.go
@@ -5,42 +5,17 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
-	"time"
 
 	state "github.com/r0nk/omira/state"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
-
-func write_task_to_ledger(task_to_remove state.Task) {
-	file, err := os.OpenFile("omira.ledger", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	writer := bufio.NewWriter(file)
-	defer writer.Flush()
-	bytes, err := yaml.Marshal(task_to_remove)
-	if err != nil {
-		log.Fatal(err)
-	}
-	n := time.Now()
-	fmt.Fprintf(writer, "%s\t%s\n", n.Format("2006-01-02T15:04-07:00"), task_to_remove.Name)
-	s := strings.Replace(string(bytes), "\n", "\n\t", -1)
-	s = strings.Replace(s, "name: "+task_to_remove.Name+"\n", "", -1)
-	fmt.Fprintln(writer, s)
-}
 
 // finishCmd represents the finish command
 var finishCmd = &cobra.Command{
 	Use:   "finish",
 	Short: "Mark a task as complete",
-	Long: `Finish a task, removing it from the task tree and adding it to the ledger.
+	Long: `Finish a task, marking it as complete.
 For example:
 	omira finish dance
 `,
@@ -60,12 +35,7 @@ For example:
 			fmt.Printf("Could not find task;%s\n", args[0])
 			return
 		}
-		err := os.Remove("tasks/" + task_to_remove.Name)
-		if err != nil {
-			fmt.Printf("Could not remove old task.\n")
-			log.Fatal(err)
-		}
-		write_task_to_ledger(task_to_remove)
+		//mark task as complete
 	},
 }
 
