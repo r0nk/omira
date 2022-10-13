b6b767e3db59e4064bf8a9c9fab018c9fb16a998
r0nk <r0nk@simplecpu.com>
2022-10-13 13:14:55 -0500

    left takes finished tasks into account

--- cmd/left.go
+++ cmd/left.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/jedib0t/go-pretty/text"
 	state "github.com/r0nk/omira/state"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +17,12 @@ Example:
 	omira finish $(omira left | fzf)
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, t := range state.Schedule(8) {
+		worked_minutes := 0.0
+		for _, t := range state.Tasks_finished_on(time.Now()) {
+			worked_minutes += t.Time_estimate.Minutes()
+			fmt.Printf("%s\n", text.Colors{text.FgHiBlack}.Sprintf("%s", t.Name))
+		}
+		for _, t := range state.Schedule(8 - (worked_minutes / 60)) {
 			fmt.Printf("%s\n", t.Name)
 		}
 	},
