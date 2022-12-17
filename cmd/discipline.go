54b3c67e2b483c686070d6d6414c1c553d1164c1
r0nk <r0nk@simplecpu.com>
2022-12-17 11:33:58 -0600

    added ratio to discipline measure

--- cmd/discipline.go
+++ cmd/discipline.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -13,6 +9,7 @@ import (
 )
 
 var date_string string
+var ratio bool
 
 // disciplineCmd represents the discipline command
 var disciplineCmd = &cobra.Command{
@@ -20,11 +17,20 @@ var disciplineCmd = &cobra.Command{
 	Short: "Print the percentage of tasks complete today.",
 	Long:  `Output the percentage of tasks complete to day, to be used in scripts or conkys.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%f\n", state.Discipline(time.Now()))
+		discipline := state.Discipline(time.Now())
+		if ratio {
+			minute := float64(time.Now().Minute())
+			hour := float64(time.Now().Hour()) + (minute / 60)
+			day_percentage := float64(100.0 * (hour - 9) / 8)
+			fmt.Printf("%f\n", discipline/day_percentage)
+		} else {
+			fmt.Printf("%f\n", discipline)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(disciplineCmd)
 	disciplineCmd.Flags().StringVarP(&date_string, "date", "d", "", "Set the date to get discipline for.")
+	disciplineCmd.Flags().BoolVarP(&ratio, "ratio", "r", false, "Get the ratio of discipline versus time worked for the day.")
 }
